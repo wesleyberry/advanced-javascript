c0421dc967ce201d6e5c61577cf79415f2679505
Wesley Berry <wesleyberry52@gmail.com>
2019-12-09 11:23:51 -0600

    created prototypes, used object.create and added methods across all instances

--- prototypes.js
+++ prototypes.js
@@ -1,5 +1,25 @@
-let animal = {};
-animal.name = 'Leo';
-animal.energy = 10;
+// A prototype is a property of a function that points to an object
+function Animal (name, energy) {
+    let animal = Object.create(Animal.prototype);
+    animal.name = name;
+    animal.energy = energy;
 
-console.log(animal);
+    return animal;
+}
+
+Animal.prototype.eat = function(amount) {
+    console.log(`${this.name} is eating`)
+    this.energy += amount
+};
+Animal.prototype.sleep = function(length) {
+    console.log(`${this.name} is sleeping`)
+    this.energy += length
+};
+Animal.prototype.play = function (length) {
+    console.log(`${this.name} is playing`)
+    this.energy -= length
+};
+
+const leo = Animal('Leo', 7);
+const snoop = Animal('Snoop', 5);
+leo.play(5);
