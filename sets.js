3c188f6f830e80a07504ea04abda9c92a574ceff
Wesley Berry <wesleyberry52@gmail.com>
2019-12-13 11:52:03 -0600

    using sets to arrays

---
+++ sets.js
@@ -0,0 +1,20 @@
+// var names = new Set();
+
+// names.add('Shaun').add('Ryu').add('Crystal');
+// names.delete('Crystal');
+// console.log(names.has('Ryu'));
+
+// console.log(names.size);
+
+// console.log(names);
+
+var ninjas = ['Shaun', 'Crystal', 'Yoshi', 'Ryu', 'Yoshi', 'Ryu'];
+var refinedNinjas = new Set(ninjas);
+console.log(refinedNinjas);
+var ninjas = [...refinedNinjas];
+console.log(ninjas);
+
+var cats = ['Sully', 'Joey', 'Winston', 'Lucille', 'Joey', 'Winston'];
+var refinedCats = new Set(cats);
+var catsArr = [...refinedCats];
+console.log(catsArr);
