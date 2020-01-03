628c4220fb8a29077bae36e263a7f9f9f1f9c29b
Wesley Berry <wesleyberry52@gmail.com>
2020-01-03 13:57:05 -0600

    filter method practice

--- higherOrderFunctions/main.js
+++ higherOrderFunctions/main.js
@@ -8,6 +8,36 @@ const companies = [
     {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-  ];
+];
   
-  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// for(let i = 0; i < companies.length; i++) {
+//     console.log(companies[i]);
+// }
+
+// forEach
+// companies.forEach(function(company) {
+//     console.log(company);
+// });
+
+// filter
+// let canDrink = ages.filter(age => age >= 21);
+// console.log(canDrink);
+
+// let retailCompany = companies.filter(company => company.category == 'Retail');
+// console.log(retailCompany);
+
+// let eightiesCompanies = companies.filter(company => company.start >= 1980 && company.start < 1990);
+// console.log(eightiesCompanies);
+
+let lastTenYearsCompanies = companies.filter(company => (company.end - company.start > 10));
+console.log(lastTenYearsCompanies);
+
+// map
+
+
+
+// sort
+
+// reduce
