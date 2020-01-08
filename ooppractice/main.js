75f60f6116c6206c951b7221f05216eed5097168
Wesley Berry <wesleyberry52@gmail.com>
2020-01-08 13:54:11 -0600

    get file extension

--- ooppractice/main.js
+++ ooppractice/main.js
@@ -1,11 +1,17 @@
 // Question one
-function checkTwoNums(a, b) {
-    if(a + b === 100) {
-        return true;
-    } 
-    if(a === 100 || b === 100) {
-        return true;
-    }
-}
+// function checkTwoNums(a, b) {
+//     if(a + b === 100) {
+//         return true;
+//     } 
+//     if(a === 100 || b === 100) {
+//         return true;
+//     }
+// }
 
-console.log(checkTwoNums(100, 99));
+// console.log(checkTwoNums(100, 99));
+
+// Write a JS program to get the extension of a filename
+const getFileExtension = str => str.slice(str.lastIndexOf('.'));
+
+console.log(getFileExtension('index.html'));
+console.log(getFileExtension('webpack.config.json'));
