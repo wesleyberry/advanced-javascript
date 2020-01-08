12b0d7f728b1d88e3dde5070534858d343a8bb1f
Wesley Berry <wesleyberry52@gmail.com>
2020-01-08 13:58:32 -0600

    moved chars forward

--- ooppractice/main.js
+++ ooppractice/main.js
@@ -11,7 +11,15 @@
 // console.log(checkTwoNums(100, 99));
 
 // Write a JS program to get the extension of a filename
-const getFileExtension = str => str.slice(str.lastIndexOf('.'));
+// const getFileExtension = str => str.slice(str.lastIndexOf('.'));
 
-console.log(getFileExtension('index.html'));
-console.log(getFileExtension('webpack.config.json'));
+// console.log(getFileExtension('index.html'));
+// console.log(getFileExtension('webpack.config.json'));
+
+// Replace every character in a given string with the character following it in the alphabet.
+const moveCharsForward = str => 
+    str.split('')
+        .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+        .join('');
+
+console.log(moveCharsForward('abcd'));
