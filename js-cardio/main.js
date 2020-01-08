18db78f8b4d97cc54d2c1b36f0005efb29b1ef6f
Wesley Berry <wesleyberry52@gmail.com>
2020-01-08 14:53:26 -0600

    reversed an int

--- js-cardio/main.js
+++ js-cardio/main.js
@@ -4,10 +4,22 @@
 
 // console.log(reverseString('hello'));
 // -------------------------------------------------
-function isPalindrome(str) {
-    if(str == str.split('').reverse().join('')) {
-        return true;
-    }
-}
 
-console.log(isPalindrome('racecar'));
+// function isPalindrome(str) {
+//     if(str == str.split('').reverse().join('')) {
+//         return true;
+//     }
+// }
+
+// console.log(isPalindrome('racecar'));
+// -------------------------------------------------
+
+function reverseInt(int) {
+    return parseInt(
+        int.toString()
+        .split('')
+        .reverse()
+        .join('')
+        );
+}
+console.log(reverseInt(10009));
