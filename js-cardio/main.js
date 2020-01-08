399ae58c45890983656870585419f65a67e05a6a
Wesley Berry <wesleyberry52@gmail.com>
2020-01-08 14:48:28 -0600

    isPalidrome

--- js-cardio/main.js
+++ js-cardio/main.js
@@ -1,5 +1,13 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString('hello'));
+// -------------------------------------------------
+function isPalindrome(str) {
+    if(str == str.split('').reverse().join('')) {
+        return true;
+    }
 }
 
-console.log(reverseString('hello'));
+console.log(isPalindrome('racecar'));
