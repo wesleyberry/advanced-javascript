1ca46b918981077a965aff23b88d8ec559b8c519
Wesley Berry <wesleyberry52@gmail.com>
2020-01-14 15:42:36 -0600

    regex review

---
+++ regex/regex.js
@@ -0,0 +1,9 @@
+// regex - a sequence of characters that describe a search pattern
+// \d -> any digit 0-9
+// \w -> capital A-Z or lowercase a-z or 0-9
+// \s -> any white space
+// . -> any character whatsoever
+
+//  \W - anything that is not a word character
+//  \S anything that is not white space
+
