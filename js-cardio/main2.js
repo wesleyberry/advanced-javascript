6c809ba18b25c3ff3e5658696499187fc5415d7b
Wesley Berry <wesleyberry52@gmail.com>
2020-01-14 11:03:52 -0600

    chunked arrays

--- js-cardio/main2.js
+++ js-cardio/main2.js
@@ -8,3 +8,18 @@
 //     return longestWordArr;
 // }
 // console.log(longestWord('hello there, my name is chicka chicka Slim Shady'));
+
+function chunkArray(arr, len) {
+    const chunkedArr = [];
+    
+    let i = 0;
+
+    while(i < arr.length) {
+        chunkedArr.push(arr.slice(i, i + len));
+        i += len;
+    }
+
+    return chunkedArr;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3));
