1ab31d4f008f8c269d905c08eb4aa0bbdbf79bc9
Wesley Berry <wesleyberry52@gmail.com>
2020-01-14 10:58:42 -0600

    found longest word in a string

---
+++ js-cardio/main2.js
@@ -0,0 +1,10 @@
+// function longestWord(sen) {
+//     const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+//     const sorted = wordArr.sort((a, b) => b.length - a.length);
+
+//     const longestWordArr = sorted.filter(word => word.length === sorted[0].length);
+
+//     return longestWordArr;
+// }
+// console.log(longestWord('hello there, my name is chicka chicka Slim Shady'));
