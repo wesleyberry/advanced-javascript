ed1f7516ba040c55f07688597d5cc614441f1396
Wesley Berry <wesleyberry52@gmail.com>
2020-01-17 12:51:40 -0600

    serving api data

--- express-rest/index.js
+++ express-rest/index.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
