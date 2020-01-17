fa4dfe5c66c72ceb3fa694d2aff14e066cf08748
Wesley Berry <wesleyberry52@gmail.com>
2020-01-17 15:29:02 -0600

    can delete and update data

--- express-rest/index.js
+++ express-rest/index.js
@@ -29,14 +29,9 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if(error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
 
@@ -54,5 +49,37 @@ app.get('/api/courses/:id', (req, res) => {
     if(!course) res.status(404).send('The course with the given ID was not found.');
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found.');
+
+    const { error } = validateCourse(req.body);
+    if(error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c.id === parseInt(id));
+    if(!course) res.status(404).send('The course with the given ID was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course.body, schema);
+}
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
