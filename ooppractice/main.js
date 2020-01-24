101283b8b8605b056075706da928984515b09ef3
Wesley Berry <wesleyberry52@gmail.com>
2020-01-24 12:31:58 -0600

    more algorithm practice

--- ooppractice/main.js
+++ ooppractice/main.js
@@ -11,18 +11,18 @@
 // console.log(checkTwoNums(100, 99));
 
 // Write a JS program to get the extension of a filename
-// const getFileExtension = str => str.slice(str.lastIndexOf('.'));
+const getFileExtension = str => str.slice(str.lastIndexOf('.'));
 
-// console.log(getFileExtension('index.html'));
-// console.log(getFileExtension('webpack.config.json'));
+console.log(getFileExtension('index.html'));
+console.log(getFileExtension('webpack.config.json'));
 
 // Replace every character in a given string with the character following it in the alphabet.
-// const moveCharsForward = str => 
-//     str.split('')
-//         .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
-//         .join('');
+const moveCharsForward = str => 
+    str.split('')
+        .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+        .join('');
 
-// console.log(moveCharsForward('abcd'));
+console.log(moveCharsForward('abcd'));
 
 // Write a JS program to get the current date
 // const formatDate = (date = new Date()) => {
@@ -35,6 +35,14 @@
 // console.log(formatDate());
 
 // Add 'New!' in front of given string. if it begins with 'new', already return the string
-// const addNew = str => str.indexOf('New!') === 0 ? str : `New! ${str}`;
+const addNew = str => str.indexOf('New!') === 0 ? str : `New! ${str}`;
 
 // console.log(addNew('New! Offers'));
+
+let formatDate = (date = new Date()) => {
+    let day = date.getDay() + 1;
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
+console.log(formatDate());
