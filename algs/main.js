efa78fd3633b53d404534d7755f39d245af98739
Wesley Berry <wesleyberry52@gmail.com>
2020-02-06 14:38:49 -0600

    more algorithms practice

--- algs/main.js
+++ algs/main.js
@@ -38,7 +38,7 @@ repeatStringNumTimes = (str, num) => num < 0 ? '' : str.repeat(num);
 console.log(repeatStringNumTimes('*', 3));
 console.log(repeatStringNumTimes('+', 10));
 // ----------------------------------------------------
-truncateString = (str, num) => num < 3 ? '' :str.substring(0, num - 3) + '...';
+truncateString = (str, num) => num < 3 ? '' : str.substring(0, num - 3) + '...';
 console.log(truncateString('A-tisket a-tasket a green and yellow basket', 11));
 console.log(truncateString('Peter Piper picked a pack of pickled peppers', 14));
 console.log(truncateString('Peter Piper picked a pack of pickled peppers', 2));
@@ -63,3 +63,47 @@ console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
 slasher = (arr, howMany) => arr.splice(0, howMany);
 console.log(slasher([1, 2, 3], 2));
 console.log(slasher([1, 2, 3, 4, 5, 6], 1));
+// ----------------------------------------------------
+mutation = arr => {
+    let firstWord = arr[0].toLowerCase();
+    let secondWord = arr[1].toLowerCase();
+    // for(let i = 0; i < secondWord.length; i++) {
+    //     if(firstWord.indexOf(secondWord[i]) === -1) return false;
+    // }
+    for(var letter of secondWord) {
+        if(firstWord.indexOf(letter) === -1) return false;
+    }
+    return true;
+}
+console.log(mutation(["hello", "hey"]));
+console.log(mutation(["hello", "Hello"]));
+// ----------------------------------------------------
+bouncer = arr => arr.filter(elem => elem);
+console.log(bouncer([7, "ate", "", false, 9]));
+// ----------------------------------------------------
+function destroyer(arr) {
+    var args = Array.from(arguments);
+
+    args.splice(0, 1);
+    var targets = args;
+    let results = [];
+
+    for(var num of arr) {
+        if(targets.indexOf(num) === -1) {
+            results.push(num);
+        }
+    }
+    return results;
+}
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+// ----------------------------------------------------
+getIndexToIns = (arr, num) => {
+    arr.push(num);
+    return arr.sort((a, b) => a - b).indexOf(num);
+}
+console.log(getIndexToIns([30, 25, 40, 60], 50));
+// ----------------------------------------------------
+rot13 = str => {
+
+}
+console.log(rot13("SERR PBQR PNZC"));
